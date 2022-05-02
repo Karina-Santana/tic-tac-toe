6dcf96bec5ec9395896ef494aadd9edf662b377e
kah <karinasantanaarq@gmail.com>
2022-05-02 17:47:25 +1000

    everything is working

--- main.js
+++ main.js
@@ -43,31 +43,44 @@ savBtn2.addEventListener('click', function (event) {
 })
 
 
-function checkWinner() {
+var scorePlayer1 = document.querySelector('.score1');
+var scorePlayer2 = document.querySelector('.score2');
+scorePlayer1.textContent = 0;
+scorePlayer2.textContent = 0;
+
+var boxDivs = document.querySelectorAll('div');
+
+function removeEventListeners() {
     for (let i = 0; i < boxDivs.length; i++) {
-        if (box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X' || box4.textContent === 'X' && box5.textContent === 'X' && box6.textContent === 'X' || box7.textContent === 'X' && box8.textContent === 'X' && box9.textContent === 'X') {
-            playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (box1.textContent === 'X' && box4.textContent === 'X' && box7.textContent === 'X' || box2.textContent === 'X' && box5.textContent === 'X' && box8.textContent === 'X' || box3.textContent === 'X' && box6.textContent === 'X' && box9.textContent === 'X') {
-            playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (box1.textContent === 'X' && box5.textContent === 'X' && box9.textContent === 'X' || box3.textContent === 'X' && box5.textContent === 'X' && box7.textContent === 'X') {
-            playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (box1.textContent === 'O' && box2.textContent === 'O' && box3.textContent === 'O' || box4.textContent === 'O' && box5.textContent === 'O' && box6.textContent === 'O' || box7.textContent === 'O' && box8.textContent === 'O' && box9.textContent === 'O') {
-            playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (box1.textContent === 'O' && box4.textContent === 'O' && box7.textContent === 'O' || box2.textContent === 'O' && box5.textContent === 'O' && box8.textContent === 'O' || box3.textContent === 'O' && box6.textContent === 'O' && box9.textContent === 'O') {
-            playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (box1.textContent === 'O' && box5.textContent === 'O' && box9.textContent === 'O' || box3.textContent === 'O' && box5.textContent === 'O' && box7.textContent === 'O') {
-            playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
-            boxDivs[i].style.pointerEvents = "none";
-        } else if (counter === 9) {
-            playerWinner.textContent = "That's a draw!"
-            boxDivs[i].style.pointerEvents = "none";
-        }
+        boxDivs[i].style.pointerEvents = 'none';
     }
+}
+
+function checkWinner() {
+
+    if (box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X' || box4.textContent === 'X' && box5.textContent === 'X' && box6.textContent === 'X' || box7.textContent === 'X' && box8.textContent === 'X' && box9.textContent === 'X') {
+        playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
+        scorePlayer1.textContent = Number(scorePlayer1.textContent) + 1;
+        removeEventListeners()
+    } else if (box1.textContent === 'X' && box4.textContent === 'X' && box7.textContent === 'X' || box2.textContent === 'X' && box5.textContent === 'X' && box8.textContent === 'X' || box3.textContent === 'X' && box6.textContent === 'X' && box9.textContent === 'X') {
+        playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
+        scorePlayer1.textContent = Number(scorePlayer1.textContent) + 1;
+    } else if (box1.textContent === 'X' && box5.textContent === 'X' && box9.textContent === 'X' || box3.textContent === 'X' && box5.textContent === 'X' && box7.textContent === 'X') {
+        playerWinner.textContent = "The winner is " + player1Name.textContent + " 🏆"
+        scorePlayer1.textContent = Number(scorePlayer1.textContent) + 1;
+    } else if (box1.textContent === 'O' && box2.textContent === 'O' && box3.textContent === 'O' || box4.textContent === 'O' && box5.textContent === 'O' && box6.textContent === 'O' || box7.textContent === 'O' && box8.textContent === 'O' && box9.textContent === 'O') {
+        playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
+        scorePlayer2.textContent = Number(scorePlayer2.textContent) + 1;
+    } else if (box1.textContent === 'O' && box4.textContent === 'O' && box7.textContent === 'O' || box2.textContent === 'O' && box5.textContent === 'O' && box8.textContent === 'O' || box3.textContent === 'O' && box6.textContent === 'O' && box9.textContent === 'O') {
+        playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
+        scorePlayer2.textContent = Number(scorePlayer2.textContent) + 1;
+    } else if (box1.textContent === 'O' && box5.textContent === 'O' && box9.textContent === 'O' || box3.textContent === 'O' && box5.textContent === 'O' && box7.textContent === 'O') {
+        playerWinner.textContent = "The winner is " + player2Name.textContent + " 🏆"
+        scorePlayer2.textContent = Number(scorePlayer2.textContent) + 1;
+    } else if (counter === 9) {
+        playerWinner.textContent = "That's a draw!"
+    }
+
 }
 
 
@@ -78,17 +91,19 @@ function checkWinner() {
 var parentBox = document.querySelector('.game-section')
 function turn() {
     parentBox.addEventListener('click', function (event) {
-        counter++
+        console.log(counter)
         if (event.target.tagName === 'DIV') {
             console.log('player turn is ' + playerTurn)
             if (playerTurn === 'X') {
                 event.target.textContent = 'X'
                 event.target.style.backgroundColor = '#B5D35B'
                 playerTurn = 'O'
+                counter++
             } else {
                 event.target.textContent = 'O'
                 event.target.style.backgroundColor = '#F0BE2C'
                 playerTurn = 'X'
+                counter++
             }
             checkWinner()
 
@@ -108,6 +123,7 @@ for (let i = 0; i < boxDivs.length; i++) {
         boxDivs[i].style.pointerEvents = "none";
         if (boxDivs[i].textContent === 'X' || boxDivs[i].textContent === 'O') {
             console.log(`You can not click here!`)
+            counter = 1;
         }
     })
 }
@@ -117,10 +133,10 @@ for (let i = 0; i < boxDivs.length; i++) {
 
 //para o botao reset, usar isso para deixar as divs clicaveis de novo:  .style.pointerEvents = 'auto';
 
-var resetBtn = document.querySelector('.play-again')
+var againBtn = document.querySelector('.play-again')
 
 for (let i = 0; i < boxDivs.length; i++) {
-    resetBtn.addEventListener('click', function (event) {
+    againBtn.addEventListener('click', function (event) {
         boxDivs[i].textContent = "";
         boxDivs[i].style.backgroundColor = 'white';
         boxDivs[i].style.pointerEvents = 'auto';
@@ -131,6 +147,19 @@ for (let i = 0; i < boxDivs.length; i++) {
 }
 
 
+var resetBtn = document.querySelector('.reset-scores')
 
-//como deixar a posicao da secao winer permanente
-//como alterar a fonte dos x e o
+for (let i = 0; i < boxDivs.length; i++) {
+    resetBtn.addEventListener('click', function (event) {
+        boxDivs[i].textContent = "";
+        boxDivs[i].style.backgroundColor = 'white';
+        boxDivs[i].style.pointerEvents = 'auto';
+        playerTurn = 'X';
+        counter = 0;
+        playerWinner.textContent = ""
+        scorePlayer1.textContent = 0;
+        scorePlayer2.textContent = 0;
+    })
+}
+
+
