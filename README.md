49df098ef122a82e5d55f94e746ff8684a17b61f
kah <karinasantanaarq@gmail.com>
2022-05-02 10:25:36 +1000

    read me

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 ## Sumary
 
+This is a Tic Tac Toe game.
+
+How to play:
+
+- First add the names of the two players, save and start playing.
+- Go clicking on the boxes trying to form a combination of 3 boxes with your symbol.
+- You can combine 3 boxes horizontally, vertically or both diagonally.
+- The first player to get a combination of 3 boxes wins.
+- To play again, press the 'Play Again' button.
+
 ## Plan
 
-<img src="./images/Flowchart - Tic Tac Toe.jpeg" alt="">
+<img src="Users/karinasantana/sei-53/project1/images/Flowchart - Tic Tac Toe.jpeg" width="128">
 
 **Step1:**
 Create 2 inputs to put the players name and store them in variables.
@@ -12,12 +22,36 @@ Create 9 boxes (divs) with a class called game-section
 Create a reset button 
 
 **Step2:**
-Create an EventListener for when player 1 clicks one of the boxes the box will have an X inside, then the second player will have an O inside
+Create a variable for the player turn which will receive a 'X' value.
+Create an EventListener with an if inside. 
+The if condition is: when player turns is equal to 'X' the clicked box will have an 'X' inside, then the player turn will have a 'O' value. If player turn is equal to 'O' the clicked box will have an 'O' inside. 
 
 **Step3:**
 Create an if statement with the conditions:
-Are there 3 boxes with **X** inside in one row? If yes, print that player 1 won, if not, are there 3 boxes with **O** inside in one row? If yes, print that player 2 won, if not, are there empty boxes? If not, the game is tied and no one wins.
+If box1, box2 and box3 are equal to 'X' print that the play 1 won.
+If it is working, continue with the other conditions.
 
 **Step4**
 Do not allow a player to click 2 consecutive times
+Create a for loop with an EventListener and an if statement with the condition that if the user clicks on a box that has an 'X' value or 'O' value, print that the user cannot click there and use the pointerEvents method to not let the user to click on it anymore.
 
+**Step5**
+Create a count variable with a value of 0 and add an else if statement in the if of step 3 saying that if the count variable is equal to 9 print that nobody won.
+
+**Step6**
+Create an Event Listener for when the user clicks the save button on the input, create an element with the player's name and store it in the parent element I want.
+
+**Step7**
+Think about how to show the player's name along with that he won or that no one won without using console.log or alert, but creating an html element on the screen.
+
+**Step8**
+Think about how to reduce and clean up the code a bit. Try storing if statements conditions in variables with arrays to see if it works.
+
+**Step9**
+Create an event listener so that when the user clicks the Play Again button, all the boxes become empty. The method pointerEvents can help with that.
+
+**Step 10**
+Create a for loop and if statement to count the scores of both players
+
+**Step 11**
+Make the website responsive
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var player1 = document.querySelector('player-1-name')
 var player2 = document.querySelector('player-2-name')
 var playerTurn = 'X'
+var counter = 0
 
 var box1 = document.querySelector('.box1')
 var box2 = document.querySelector('.box2')
@@ -12,30 +13,117 @@ var box7 = document.querySelector('.box7')
 var box8 = document.querySelector('.box8')
 var box9 = document.querySelector('.box9')
 
-
+// var row1 = [box1.textContent, box2.textContent, box3.textContent]
+// var row2 = [box4.textContent, box5.textContent, box6.textContent]
+// var row3 = [box7.textContent, box8.textContent, box9.textContent]
+// var column1 = []
+// if (row1[0] === 'X' && row1[1] === 'X' && row[2] === 'X')
 
 function checkWinner() {
-    if (box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X' || box4.textContent === 'X' && box5.textContent === 'X' && box6.textContent === 'X' || box7.textContent === 'X' && box8.textContent === 'X' && box9.textContent === 'X') {
-        console.log(`${playerTurn} won!`)
+    for (let i = 0; i < boxDivs.length; i++) {
+        if (box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X' || box4.textContent === 'X' && box5.textContent === 'X' && box6.textContent === 'X' || box7.textContent === 'X' && box8.textContent === 'X' && box9.textContent === 'X') {
+            console.log('Player 1 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (box1.textContent === 'X' && box4.textContent === 'X' && box7.textContent === 'X' || box2.textContent === 'X' && box5.textContent === 'X' && box8.textContent === 'X' || box3.textContent === 'X' && box6.textContent === 'X' && box9.textContent === 'X') {
+            console.log('Player 1 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (box1.textContent === 'X' && box5.textContent === 'X' && box9.textContent === 'X' || box3.textContent === 'X' && box5.textContent === 'X' && box7.textContent === 'X') {
+            console.log('Player 1 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (box1.textContent === 'O' && box2.textContent === 'O' && box3.textContent === 'O' || box4.textContent === 'O' && box5.textContent === 'O' && box6.textContent === 'O' || box7.textContent === 'O' && box8.textContent === 'O' && box9.textContent === 'O') {
+            console.log('Player 2 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (box1.textContent === 'O' && box4.textContent === 'O' && box7.textContent === 'O' || box2.textContent === 'O' && box5.textContent === 'O' && box8.textContent === 'O' || box3.textContent === 'O' && box6.textContent === 'O' && box9.textContent === 'O') {
+            console.log('Player 2 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (box1.textContent === 'O' && box5.textContent === 'O' && box9.textContent === 'O' || box3.textContent === 'O' && box5.textContent === 'O' && box7.textContent === 'O') {
+            console.log('Player 2 won!')
+            boxDivs[i].style.pointerEvents = "none";
+        } else if (counter === 9) {
+            console.log(`Nobody won!`)
+            boxDivs[i].style.pointerEvents = "none";
+        }
     }
 }
 
 
 
 var parentBox = document.querySelector('.game-section')
-function test() {
+function turn() {
     parentBox.addEventListener('click', function (event) {
+        counter++
         if (event.target.tagName === 'DIV') {
-            checkWinner()
+            console.log('player turn is ' + playerTurn)
             if (playerTurn === 'X') {
                 event.target.textContent = 'X'
+                event.target.style.backgroundColor = '#B5D35B'
                 playerTurn = 'O'
             } else {
                 event.target.textContent = 'O'
+                event.target.style.backgroundColor = '#F0BE2C'
                 playerTurn = 'X'
             }
+            checkWinner()
+
         }
     })
 }
 
-test()
+turn()
+
+
+//NOT ALLOW TO CLICK TWICE IN THE SAME BOX:
+
+var boxDivs = document.querySelectorAll('div');
+
+for (let i = 0; i < boxDivs.length; i++) {
+    boxDivs[i].addEventListener('click', function (event) {
+        boxDivs[i].style.pointerEvents = "none";
+        counter = 0;
+        if (boxDivs[i].textContent === 'X' || boxDivs[i].textContent === 'O') {
+            console.log(`You can not click here!`)
+        }
+    })
+}
+
+
+
+//INPUTS AND SAVE BUTTONS:
+
+var savBtn1 = document.querySelector('.save-1')  //button
+var player1 = document.querySelector('player-1-name') //input
+var secWinner = document.querySelector('.winner') // parent
+
+var savBtn2 = document.querySelector('.save-2') //button
+
+var player1Name = document.querySelector('.title-player-1') // Player1: title
+
+
+savBtn1.addEventListener('click', function (event) {
+    event.preventDefault()
+    // var nameSaved = document.createElement('p');
+    // nameSaved.textContent = `Player: ${player1.value}`;
+    // player1.value = '';
+    // secWinner.appendChild(nameSaved);
+    player1.textContent = player1Name.textContent;
+    //posso alterar o placeholder tbm? Para 'Change your name here'
+
+})
+
+
+
+//para o botao reset, usar isso para deixar as divs clicaveis de novo:  .style.pointerEvents = 'auto';
+
+var resetBtn = document.querySelector('.play-again')
+
+for (let i = 0; i < boxDivs.length; i++) {
+    resetBtn.addEventListener('click', function (event) {
+        boxDivs[i].textContent = "";
+        boxDivs[i].style.backgroundColor = 'white';
+        boxDivs[i].style.pointerEvents = 'auto';
+        playerTurn = 'X';
+        counter = 0;
+    })
+}
+
+
